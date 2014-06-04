d3ea690f66b4f2cbee68bf791ce4a9cb25f9afd9
Matt Gardner <gmat@gmat-macbookpro2.roam.corp.google.com>
2014-06-03 18:37:51 -0700

    removed users, renamed routes to controllers, added models

--- app/app.js
+++ app/app.js
@@ -5,9 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -22,8 +19,8 @@ app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', require('./controllers/index'));
+app.use('/data.txt', require('./models/quizData'));
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
